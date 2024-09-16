2421b2e5c21cf7484fa8e76e26083b0c434865a7
Harisshks <harisshhari8@gmail.com>
2024-09-16 14:42:48 +0530

    Update callback.js

--- callback.js
+++ callback.js
@@ -25,24 +25,51 @@
 //     }
 // }
 // result("adfsa")
-async function API() {
-    try{
-    const response = await fetch("https://66e5271a5cc7f9b6273c6e28.mockapi.io/User")
-    const data =await response.json()
-    // console.table(data)
-    // console.log(response)
-    return data
-    }
-    catch(error){
-        console.log(error)
-    }
+// async function API() {
+//     try{
+//     const response = await fetch("https://66e5271a5cc7f9b6273c6e28.mockapi.io/User")
+//     const data =await response.json()
+//     // console.table(data)
+//     // console.log(response)
+//     return data
+//     }
+//     catch(error){
+//         console.log(error)
+//     }
+// }
+
+// async function APIDATA() {
+//     const apidata = await API()
+//     // const structuredata = apidata.map((data) => {console.log(data)})
+//     const activeusers = await apidata.filter((data) => data.isactive)
+//     console.log(activeusers)
+//     // console.log(apidata)
+// }
+// APIDATA()
+
+function bot(){
+    return new Promise((resolve,reject) => {
+        const a = true
+        setTimeout(()=>{
+            if(a){
+                resolve("Success")
+            }
+            else{
+                reject("Error")
+            }
+        },3000)
+    })
 }
 
-async function APIDATA() {
-    const apidata = await API()
-    // const structuredata = apidata.map((data) => {console.log(data)})
-    const activeusers = await apidata.filter((data) => data.isactive)
-    console.log(activeusers)
-    // console.log(apidata)
-}
-APIDATA()
+// async function datax() {
+//     try{
+//         const response = await bot()
+//         console.log(response)
+//     }
+//     catch(e){
+//         console.log(e)
+//     }
+// }
+// datax()
+
+bot().then(data=>{console.log(data)}).catch(e => {console.log(e)})
